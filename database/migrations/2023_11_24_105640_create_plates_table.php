9dbfe2639a346c26d3e18f6264766f32f4f5543f
Samuele-Benato <samuele.benato@gmail.com>
2023-11-24 12:58:54 +0100

    feat : plate migration

---
+++ database/migrations/2023_11_24_105640_create_plates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('restaurants_id')->nullable()->constrained();
+            $table->string('name', 50);
+            $table->text('ingredients');
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->decimal('price');
+            $table->tinyInteger('visibility');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plates');
+    }
+};
