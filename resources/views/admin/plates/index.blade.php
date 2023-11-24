d2dbca66ae12cea3580cb441b15a4928ee52c239
Samuele-Benato <samuele.benato@gmail.com>
2023-11-24 19:59:19 +0100

    fix : index

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -23,6 +23,18 @@
                         <div class="food__content">
                             <div class="food__title">
                                 <h1 class="food__heading">{{ $plate->name }} 🍽️</h1>
+                                <div class="fw-bold">visibile</div>
+                                <form action="{{ route('admin.plates.visibility', $plate) }}" method="POST"
+                                    id="form-visibility-{{ $plate->id }}">
+                                    @method('PATCH')
+                                    @csrf
+
+                                    <label class="switch">
+                                        <input type="checkbox" name="visibility"
+                                            @if ($plate->visibility) checked @endif>
+                                        <span class="slider round checkbox-visibility" data-id="{{ $plate->id }}"></span>
+                                    </label>
+                                </form>
                                 {{-- <div class="food__tag food__tag--1">#vegetarian</div>
                                 <div class="food__tag food__tag--2">#italian</div> --}}
                             </div>
@@ -31,15 +43,33 @@
                             </p>
                             <div>
                                 <p class="food__detail"><span class="emoji">👨‍🍳</span>{{ $plate->ingredients }}</p>
-                                <p class="food__detail"><span class="emoji">💶</span>{{ $plate->price }} €</p>
+                                <p class="food__detail"><span class="emoji">💶</span>{{ $plate->price }} €
+                                </p>
                                 {{-- <p class="food__detail"><span class="emoji">⭐️</span>4.7 / 5</p> --}}
                             </div>
                         </div>
-                        <div class="food__price"><a href="{{ route('admin.plates.show', $plate) }}">🔎</a></div>
+                        <div class="food__price">
+                            <a class="my2" href="{{ route('admin.plates.show', $plate) }}">🔎</a>
+                            <a class="my2" href="{{ route('admin.plates.edit', $plate) }}">🖊️</a>
+                        </div>
                     </figure>
                 @endforeach
             </div>
         </div>
-
     </div>
+@endsection
+@section('scripts')
+    <script>
+        const checkboxesVisibility = document.getElementsByClassName('checkbox-visibility');
+        console.log(checkboxesVisibility);
+
+
+        for (checkbox of checkboxesVisibility) {
+            checkbox.addEventListener('click', function() {
+                const idPlate = this.getAttribute('data-id');
+                const form = document.getElementById('form-visibility-' + idPlate);
+                form.submit();
+            })
+        }
+    </script>
 @endsection
