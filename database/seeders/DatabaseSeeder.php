1960f4064b54d71190f3a702bfba4dfe105addda
Samuele-Benato <samuele.benato@gmail.com>
2023-11-27 09:58:32 +0100

    fix : web.route

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([PlateSeeder::class, TypeSeeder::class, RestaurantSeeder::class, RestaurantTypeSeeder::class, AdminSeeder::class]);
+        $this->call([
+            AdminSeeder::class,
+            PlateSeeder::class,
+            TypeSeeder::class,
+            RestaurantSeeder::class,
+            RestaurantTypeSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
