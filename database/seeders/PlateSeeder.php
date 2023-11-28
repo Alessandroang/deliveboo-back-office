2f765cbd9f56c4db94994ee0e5bb38ad36696f65
filippo199600 <pippotartaglia@gmail.com>
2023-11-28 09:40:39 +0100

    fix plate seeder

--- database/seeders/PlateSeeder.php
+++ database/seeders/PlateSeeder.php
@@ -9,7 +9,6 @@ use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Arr;
 
 class PlateSeeder extends Seeder
 {
@@ -23,22 +22,21 @@ class PlateSeeder extends Seeder
         // $categories = Category::all()->pluck('id')->toArray();
         // $categories[] = null;
 
-            $restaurant = Restaurant::first()->id;
+        $restaurant = Restaurant::first()->id;
 
-            for ($i = 0; $i < 15; $i++) {
-                // $category_id = Arr::random($categories);
-                $plate = new Plate;
-                // $plate->category_id = $category_id;
-                $plate->restaurant_id = Arr::random($restaurant);
-                $plate->name = $faker->words(2, true);
-                $plate->ingredients = implode(', ', $faker->words(5));
-                $plate->description = $faker->paragraph(5, true);
-                $plate->image = $faker->imageUrl(300, 200);
-                $plate->price = $faker->randomFloat(1, 8, 20);
-                $plate->visibility = $faker->numberBetween(0, 1);
-                $plate->save();
-            }
+        for ($i = 0; $i < 15; $i++) {
+            // $category_id = Arr::random($categories);
+            $plate = new Plate;
+            // $plate->category_id = $category_id;
+            $plate->restaurant_id = Arr::random($restaurant);
+            $plate->name = $faker->words(2, true);
+            $plate->ingredients = implode(', ', $faker->words(5));
+            $plate->description = $faker->paragraph(5, true);
+            $plate->image = $faker->imageUrl(300, 200);
+            $plate->price = $faker->randomFloat(1, 8, 20);
+            $plate->visibility = $faker->numberBetween(0, 1);
+            $plate->save();
         }
-
     }
+
 }
