a0cdb6b0d4907ae806f1d70aa2257a6dd1c7f716
Samuele-Benato <samuele.benato@gmail.com>
2023-11-28 09:41:43 +0100

    fix : plateSeeder

--- database/seeders/PlateSeeder.php
+++ database/seeders/PlateSeeder.php
@@ -8,8 +8,7 @@ use Faker\Generator as Faker;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Arr;
+
 
 class PlateSeeder extends Seeder
 {
@@ -22,14 +21,13 @@ class PlateSeeder extends Seeder
     {
         // $categories = Category::all()->pluck('id')->toArray();
         // $categories[] = null;
-
-        $restaurant = Restaurant::first()->id;
+        $restaurantId = Restaurant::pluck('id')->random();
 
         for ($i = 0; $i < 15; $i++) {
             // $category_id = Arr::random($categories);
             $plate = new Plate;
             // $plate->category_id = $category_id;
-            $plate->restaurant_id = Arr::random($restaurant);
+            $plate->restaurant_id = $restaurantId;
             $plate->name = $faker->words(2, true);
             $plate->ingredients = implode(', ', $faker->words(5));
             $plate->description = $faker->paragraph(5, true);
