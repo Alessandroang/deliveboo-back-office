6e452e870e2750cdf38013ee80831a589b5d9265
Samuele-Benato <samuele.benato@gmail.com>
2023-12-05 13:13:42 +0100

    fix : homepage

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,12 +3,11 @@
 
 @section('content')
     <section class="container mt-5">
-        <h1>{{ $title }}</h1>
-        <h2>Lista dei Ristoranti</h2>
+        <h2 class="mb-4">Benvenuto! ad oggi ci ha scelti :</h2>
         <ul>
             @foreach ($restaurants as $restaurant)
-                <li>
-                    <h2>{{ $restaurant->name }}</h2>
+                <li class="home_li">
+                    <h3>{{ $restaurant->name }}</h3>
                     {{-- 
                      <p>{{ $restaurant->description }}</p>
                     <!-- Aggiungi un link per visualizzare il menu o fare un ordine -->
