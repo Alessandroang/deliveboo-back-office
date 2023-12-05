1472282796efb893aacff68d05de53270651ef7a
Samuele-Benato <samuele.benato@gmail.com>
2023-12-05 11:19:38 +0100

    fix : Plate edit layout

--- resources/views/admin/plates/edit.blade.php
+++ resources/views/admin/plates/edit.blade.php
@@ -21,55 +21,52 @@
                 </div>
             @endif
 
-            <div class="my-3 text-danger required-fields d-inline p-2">* Questi campi sono obbligatori</div>
+            <div class="my-3 required-fields">* Questi campi sono obbligatori</div>
 
             <form action="{{ route('admin.plates.update', $plate) }}" method="POST" enctype="multipart/form-data">
                 @method('PUT')
                 @csrf
-                <div class="row">
-                    <div class="col">
-                        <div class="row">
-                            <div class="col-12 my-2">
-                                <label for="name" class="form-label fw-bold">Nome piatto*:</label>
-                                <input type="text" id="name" name="name"
-                                    class="form-control  @error('name') is-invalid @enderror" placeholder="Nome del piatto"
-                                    value="{{ old('name') ?? $plate->name }}">
-                                {{-- @error('name')
+                <div class="row gap-4">
+                    <div class="col-12 my-2">
+                        <label for="name" class="form-label fw-bold">Nome piatto*:</label>
+                        <input type="text" id="name" name="name"
+                            class="form-control  @error('name') is-invalid @enderror" placeholder="Nome del piatto"
+                            value="{{ old('name') ?? $plate->name }}">
+                        {{-- @error('name')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror --}}
-                            </div>
+                    </div>
 
-                            <div class="row">
-                                <div class="col-8">
-                                    <label for="image" class="form-label fw-bold">URL dell'immagine*:</label>
-                                    <input type="file" name="image" id="image"
-                                        class="form-control @error('image') is-invalid @enderror"
-                                        value="{{ old('image') }}">
+                    <div class="row">
+                        <div class="col-8">
+                            <label for="image" class="form-label fw-bold">URL dell'immagine*:</label>
+                            <input type="file" name="image" id="image"
+                                class="form-control @error('image') is-invalid @enderror" value="{{ old('image') }}">
 
-                                    {{-- @error('image')
+                            {{-- @error('image')
                                         <div class="alert alert-danger mt-2">{{ $message }}</div>
                                     @enderror --}}
-                                </div>
-                                <div class="my-5 text-danger required-fields d-inline p-2 w-50">L'immagine è stata
-                                    aggiornata. Si
-                                    prega di reinserirne una.</div>
-                                <div class="col-4 mt-2">
-                                    <img src="{{ $plate->image ? asset('storage/' . $plate->image) : 'https://placehold.co/400' }}"
-                                        class="img-fluid" id="image_preview">
-                                </div>
-                            </div>
+                        </div>
+                        <div class="col-4 mt-2">
+                            <img src="{{ $plate->image ? asset('storage/' . $plate->image) : 'https://placehold.co/400' }}"
+                                class="img-fluid" id="image_preview">
+                        </div>
+                        <div class="mt-2 required-fields">L'immagine è stata
+                            aggiornata. Si
+                            prega di reinserirne una.(?)</div>
+                    </div>
 
-                            <div class="col-12 my-2">
-                                <label for="price" class="form-label fw-bold">Prezzo*:</label>
-                                <input type="number" min="0" max="100" step="0.01" id="price"
-                                    name="price" class="form-control @error('price') is-invalid @enderror"
-                                    placeholder="Prezzo el piatto" value="{{ old('price') ?? $plate->price }}">
-                                {{-- @error('price')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror --}}
-                            </div>
+                    <div class="col-12 my-2">
+                        <label for="price" class="form-label fw-bold">Prezzo*:</label>
+                        <input type="number" min="0" max="100" step="0.01" id="price" name="price"
+                            class="form-control @error('price') is-invalid @enderror" placeholder="Prezzo el piatto"
+                            value="{{ old('price') ?? $plate->price }}">
+                        {{-- @error('price')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror --}}
+                    </div>
 
-                            {{-- <div class="col-6 my-2">
+                    {{-- <div class="col-6 my-2">
                             <label for="type_id" class="form-label"><strong>Tipo</strong></label>
                             <select name="type_id" id="type_id" class="form-select @error('type_id') is-invalid @enderror">
                                 <option value="">Seleziona il tipo</option>
@@ -79,32 +76,29 @@
                                     </option>
                                 @endforeach
                             </select> --}}
-                            {{-- @error('type_id')
+                    {{-- @error('type_id')
                             <div class="invalid-feedback">
                             {{ $message }}
                             </div>
                             @enderror --}}
-                            {{-- </div> --}}
+                    {{-- </div> --}}
 
-                            <div class="col-12">
-                                <label for="ingredients" class="form-label fw-bold">Ingredienti*:</label>
-                                <input type="textarea" id="ingredients" name="ingredients"
-                                    class="form-control @error('ingredients') is-invalid @enderror"
-                                    placeholder="Ingredienti del piatto"
-                                    value="{{ old('ingredients') ?? $plate->ingredients }}">
-                                {{-- @error('ingredients')
+                    <div class="col-12">
+                        <label for="ingredients" class="form-label fw-bold">Ingredienti*:</label>
+                        <input type="textarea" id="ingredients" name="ingredients"
+                            class="form-control @error('ingredients') is-invalid @enderror"
+                            placeholder="Ingredienti del piatto" value="{{ old('ingredients') ?? $plate->ingredients }}">
+                        {{-- @error('ingredients')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror --}}
-                            </div>
-                            <div class="col-12">
-                                <label for="description" class="form-label fw-bold">Descrizione</label>
-                                <textarea class="form-control" id="description" name="description" rows="2">{{ old('description') ?? $plate->description }}
+                    </div>
+                    <div class="col-12">
+                        <label for="description" class="form-label fw-bold">Descrizione</label>
+                        <textarea class="form-control" id="description" name="description" rows="2">{{ old('description') ?? $plate->description }}
                                 </textarea>
-                                {{-- @error('description')
+                        {{-- @error('description')
                             <div class="invalid-feedback">{{ $message }}</div>
                             @enderror --}}
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="d-flex justify-content-center">
