e35004ff583de082f8b82e18500c4fe7740f54c0
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-12-05 11:36:17 +0100

    fix home and navbar guest

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.guest')
 
+
 @section('content')
     <section class="container mt-5">
         <h1>{{ $title }}</h1>
@@ -8,9 +9,11 @@
             @foreach ($restaurants as $restaurant)
                 <li>
                     <h2>{{ $restaurant->name }}</h2>
-                    <p>{{ $restaurant->description }}</p>
+                    {{-- 
+                     <p>{{ $restaurant->description }}</p>
                     <!-- Aggiungi un link per visualizzare il menu o fare un ordine -->
                     <a href="{{ route('admin.plates.index', ['id' => $restaurant->id]) }}">Visualizza Menu</a>
+                    --}}
                 </li>
             @endforeach
         </ul>
