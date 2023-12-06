10db183d167e57c22e49084dbe73cb4be54189d4
Alessandroang <alessandroang@gmail.com>
2023-12-06 13:22:53 +0100

    Modify registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,12 +6,7 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Registrati') }}</div>
-                    <div id="name-error" class="text-danger"></div>
-                    <div id="lastname-error" class="text-danger"></div>
-                    <div id="email-error" class="text-danger"></div>
-                    <div id="vat-error" class="text-danger"></div>
-                    <div id="password-error" class="text-danger"></div>
-                    <div id="password-confirm-error" class="text-danger"></div>
+
                     <div class="m-3 text-danger">* Questi campi sono obbligatori</div>
                     <div class="card-body">
 
@@ -76,6 +71,9 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
+
+                                    <div id="email-error" class="text-danger"></div>
+
                                 </div>
                             </div>
 
@@ -93,25 +91,27 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
+                                    <div id="vat-error" class="text-danger"></div>
+
                                 </div>
                             </div>
 
                             <div class="mb-4 row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}
-                                    *</label>
-
+                                <label for="password"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}*</label>
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password">
-
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
+                                    <div id="password-error" class="text-danger"></div>
                                 </div>
                             </div>
+
 
                             <div class="mb-4 row">
                                 <label for="password-confirm"
@@ -122,15 +122,17 @@
                                     <input id="password-confirm" type="password"
                                         class="form-control @error('vat') is-invalid @enderror" name="password_confirmation"
                                         required autocomplete="new-password">
+                                    <div id="password-confirm-error" class="text-danger"></div>
+
                                 </div>
                             </div>
 
                             <div class="mb-4 row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="button" class="btn btn-primary" id="register-button" @click="submitForm">
+                                    <button type="button" class="btn btn-primary" id="register-button"
+                                        @click="validateAndSubmit">
                                         {{ __('Registrati') }}
                                     </button>
-
 
 
                                 </div>
@@ -166,6 +168,17 @@
         };
 
         const submitForm = () => {
+            // Controllo se la partita IVA è vuota
+            if (vat.value.trim() === "") {
+                document.getElementById('vat-error').innerText = 'La partita IVA è obbligatoria';
+                return;
+            } else if (vat.value.length !== 11) {
+                document.getElementById('vat-error').innerText = 'La partita IVA deve essere di 11 cifre';
+                return;
+            } else {
+                document.getElementById('vat-error').innerText = '';
+            }
+
             // Aggiungi qui la tua logica di validazione del form
             if (password.value !== passwordConfirm.value) {
                 document.getElementById('password-confirm-error').innerText = 'Le password non corrispondono';
@@ -174,13 +187,14 @@
                 document.getElementById('password-confirm-error').innerText = '';
             }
 
-            if (!isValidVAT(vat.value)) {
-                document.getElementById('vat-error').innerText = 'La partita IVA non è valida';
+            if (password.value.length === 0) {
+                document.getElementById('password-error').innerText = 'La password è obbligatoria';
                 return;
             } else {
-                document.getElementById('vat-error').innerText = '';
+                document.getElementById('password-error').innerText = '';
             }
-            // Se il form è valido, invia la richiesta al backend
+
+            // Se tutti i controlli sono superati, invia il form
             form.submit();
         };
 
@@ -188,202 +202,3 @@
         document.getElementById('register-button').addEventListener('click', submitForm);
     </script>
 @endsection
-
-
-
-{{-- @extends('layouts.app')
-
-@section('content')
-    <div class="container mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Registrati') }}</div>
-                    <div class="m-3 text-danger">* Questi campi sono obbligatori</div>
-                    <div class="card-body">
-
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
-
-                            <div class="mb-4 row">
-                                <label for="name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="name"
-                                        value="{{ old('name') }}" autocomplete="name" autofocus>
-
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="mb-4 row">
-                                <label for="lastname"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="lastname" type="text"
-                                        class="form-control @error('lastname') is-invalid @enderror" name="lastname"
-                                        value="{{ old('lastname') }}" autocomplete="lastname" autofocus>
-
-                                    @error('lastname')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="mb-4 row">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo e-mail') }} *</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                        class="form-control @error('email') is-invalid @enderror" name="email"
-                                        value="{{ old('email') }}" required autocomplete="email">
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="mb-4 row">
-                                <label for="vat" class="col-md-4 col-form-label text-md-right">{{ __('Partita IVA') }}
-                                    *</label>
-
-                                <div class="col-md-6">
-                                    <input id="vat" type="text"
-                                        class="form-control @error('vat') is-invalid @enderror" name="vat"
-                                        value="{{ old('vat') }}" required autocomplete="vat">
-
-                                    @error('vat')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="mb-4 row">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}
-                                    *</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="mb-4 row">
-                                <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}
-                                    *</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password"
-                                        class="form-control @error('vat') is-invalid @enderror" name="password_confirmation"
-                                        required autocomplete="new-password">
-                                </div>
-                            </div>
-
-                            <div class="mb-4 row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="button" class="btn btn-primary" id="register-button" @click="submitForm">
-                                        {{ __('Registrati') }}
-                                    </button>
-                                    <div id="error-message" class="text-danger mt-2"></div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
-
-@section('scripts')
-    <script>
-        const password = document.getElementById('password');
-        const passwordConfirm = document.getElementById('password-confirm');
-        const vat = document.getElementById('vat');
-        const form = document.querySelector('form');
-        const errorMessage = document.getElementById('error-message');
-
-        passwordConfirm.addEventListener('keyup', function() {
-            if (password.value !== passwordConfirm.value) {
-                passwordConfirm.classList.remove('is-valid');
-                passwordConfirm.classList.add('is-invalid');
-            } else {
-                passwordConfirm.classList.remove('is-invalid');
-                passwordConfirm.classList.add('is-valid');
-            }
-        });
-
-        const isValidVAT = (inputVAT) => {
-            const vatRegex = /^0?\d{11}$/;
-            return vatRegex.test(inputVAT);
-        };
-
-        const showError = (message) => {
-            errorMessage.innerHTML = message;
-        };
-
-        const submitForm = () => {
-            if (password.value !== passwordConfirm.value) {
-                showError('Le password non corrispondono');
-                return;
-            }
-
-            if (!isValidVAT(vat.value)) {
-                showError('La partita IVA non è valida');
-                return;
-            }
-{{-- // errorMessage.innerHTML = '';
-            // Invia il form in modo asincrono utilizzando Fetch API
-            // fetch(form.action, {
-            //         method: 'POST',
-            //         headers: {
-            //             'Content-Type': 'application/json',
-            //             'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
-            //             'X-Requested-With': 'XMLHttpRequest'
-            //         },
-            //         body: JSON.stringify(Object.fromEntries(new FormData(form)))
-            //     })
-            //     .then(response => response.json())
-                // .then(data => {
-                    // console.log(data);
-                    // Puoi gestire la risposta come preferisci
-//                 })
-//                 .catch(error => console.error('Error:', error));
-//         };
-
-//         document.getElementById('register-button').addEventListener('click', submitForm);
-//     </script>
-// @endsection --}}
