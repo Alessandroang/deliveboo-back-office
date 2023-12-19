0aa54cb1c6041ce59263c3718ea69089ce0bad43
Alessandroang <alessandroang94@gmail.com>
2023-12-19 22:31:29 +0100

    Add order of each restaurant

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -4,23 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use Illuminate\Support\Facades\Auth; // Aggiunto per utilizzare la facades Auth
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function showOrders()
     {
+        // Recupera l'ID del ristorante dell'utente autenticato
+        $userRestaurantId = Auth::user()->restaurant->id;
+
         // Recupera tutti gli ordini dal database con i piatti e i rispettivi restaurant_id
         $orders = Order::with('plates')->get();
 
-        // Ora puoi accedere ai restaurant_id dei piatti di ciascun ordine
-        foreach ($orders as $order) {
-            foreach ($order->plates as $plate) {
-                // $restaurantId = $plate->pivot->restaurant_id;
-                // Usa $restaurantId come necessario
-            }
-        }
-        // Passa gli ordini alla vista e restituisci la vista
-        return view('admin.orders.index', compact('orders'));
+        // Passa gli ordini e l'ID del ristorante alla vista
+        return view('admin.orders.index', compact('orders', 'userRestaurantId'));
     }
 }
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -4,69 +4,58 @@
     <div class="container">
         <h1>Elenco degli Ordini</h1>
 
-        @foreach ($orders as $order)
-            <!-- Check se l'ordine appartiene al ristorante desiderato -->
+        @php
+            $hasOrders = false;
+        @endphp
 
-            {{-- @if ($order->plate->restaurant_id) --}}
+        @forelse ($orders as $order)
             @php
-                $userId = Auth::user()->id; // Assumi che l'ID del ristorante del cliente sia accessibile tramite Auth::user()
+                $firstPlate = $order->plates->first();
+                $restaurantId = $firstPlate ? $firstPlate->restaurant->id : null;
             @endphp
 
+            @if ($restaurantId === $userRestaurantId)
+                @php
+                    $hasOrders = true;
+                @endphp
 
-            <div class="card mb-3">
-                <div class="card-header">
-                    Informazioni Cliente
-                </div>
+                <div class="card mb-3">
+                    <div class="card-header">
+                        Informazioni Cliente
+                    </div>
 
-                <div class="card-body">
-                    <h5 class="card-title">{{ $order->name }} {{ $order->lastname }}</h5>
-                    <p class="card-text">
-                        <strong>Address:</strong> {{ $order->address }} <br>
-                        <strong>Email:</strong> {{ $order->email }} <br>
-                        <strong>Phone:</strong> {{ $order->phone }} <br>
-                        <strong>Total Orders:</strong> {{ $order->total_orders }} <br>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $order->name }} {{ $order->lastname }}</h5>
+                        <p class="card-text">
+                            <strong>Address:</strong> {{ $order->address }} <br>
+                            <strong>Email:</strong> {{ $order->email }} <br>
+                            <strong>Phone:</strong> {{ $order->phone }} <br>
+                            <strong>Totale Ordine:</strong> {{ $order->total_orders }} <br>
 
-
-
-                    <ul class="list-group list-group-flush">
-                        @foreach ($order->plates as $plate)
-                            {{--  
-                                @if ($userId === $plate->restaurant_id)
-                            --}}
-
-                            <li class="list-group-item">
-                                <strong>Plates Ordered:</strong> {{ $plate->name }}
-                            </li>
-                            <!-- Visualizza l'ID del ristorante associato al piatto-->
-                            <li class="list-group-item">
-                                <strong>Quantità: </strong> {{ $plate->pivot->quantity }}
-
-                            </li>
-
-                            <li class="list-group-item">
-                                <strong>Date Orders:</strong> {{ $plate->pivot->created_at }} <br>
-                            </li>
-                            {{-- 
-                                @else
-                                <strong>Non ci sono ordini</strong>
-                                @endif 
-                                --}}
-                            @if (count($order->plates) < 2)
-                                <li class="list-group-item"> <strong>Id Restaurant from Plate:</strong>
-                                    {{ $plate->restaurant_id }}
+                        <ul class="list-group list-group-flush">
+                            @foreach ($order->plates as $plate)
+                                <li class="list-group-item">
+                                    <strong>Piatto ordinato:</strong> {{ $plate->name }}
                                 </li>
-                            @endif
-                        @endforeach
-                    </ul>
+                                <li class="list-group-item">
+                                    <strong>Quantità:</strong> {{ $plate->pivot->quantity }}
+                                </li>
+                                <li class="list-group-item">
+                                    <strong>Data Ordinazione:</strong> {{ $plate->pivot->created_at }} <br>
+                                </li>
+                            @endforeach
+                        </ul>
 
-                    @if (count($order->plates) < 2)
-                        <strong>Name Restaurant:</strong> {{ $plate->restaurant->name }}
-                    @endif
-
-                    </p>
+                        <strong>Nome Ristorante:</strong> {{ $firstPlate->restaurant->name }}
+                        </p>
+                    </div>
                 </div>
-            </div>
-            {{--  @endif --}}
-        @endforeach
+            @endif
+        @empty
+            {{-- Aggiunto controllo se non ci sono ordini --}}
+            @if (!$hasOrders)
+                <p>Nessun ordine disponibile per il tuo ristorante.</p>
+            @endif
+        @endforelse
     </div>
 @endsection
