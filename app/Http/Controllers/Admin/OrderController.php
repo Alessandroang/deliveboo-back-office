a1c0d53bb6bb30e2012822c0bdc7e0b3e22844c1
Alessandroang <alessandroang94@gmail.com>
2024-01-11 18:15:58 +0100

    Add final statistics

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -26,16 +26,22 @@ class OrderController extends Controller
 
     public function orderStatistics()
     {
-        // Calcola le statistiche degli ordini
-        $statistics = $this->calculateOrderStatistics();
+        // Recupera l'ID del ristorante dell'utente autenticato
+        $userRestaurantId = Auth::user()->restaurant->id;
+
+        // Calcola le statistiche degli ordini solo per il ristorante dell'utente autenticato
+        $statistics = $this->calculateOrderStatistics($userRestaurantId);
 
         // Restituzione della risposta in formato JSON
         return view('admin.orders.statistics', compact('statistics'));
     }
 
-    private function calculateOrderStatistics()
+    private function calculateOrderStatistics($restaurantId)
     {
-        return Order::select(DB::raw('MONTH(created_at) as month'), DB::raw('YEAR(created_at) as year'), DB::raw('COUNT(id) as total_orders'), DB::raw('SUM(total_orders) as total_quantity'), DB::raw('SUM(total_orders) as total_sales'))
+        return Order::whereHas('plates', function ($query) use ($restaurantId) {
+            $query->where('restaurant_id', $restaurantId);
+        })
+            ->selectRaw('MONTH(created_at) as month, YEAR(created_at) as year, COUNT(id) as total_orders, SUM(total_orders) as total_quantity, SUM(total_orders) as total_sales')
             ->groupBy(DB::raw('YEAR(created_at)'), DB::raw('MONTH(created_at)'))
             ->get();
     }
